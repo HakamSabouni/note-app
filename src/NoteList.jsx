2b50c9bdd142bd195cd7a93e0fb3c78c38d85322
Hakam <hakam.sabouni@gmail.com>
2025-10-05 13:14:02 +0200

    changed

--- src/NoteList.jsx
+++ src/NoteList.jsx
@@ -2,7 +2,7 @@ import Note from './Note';
 
 const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
-    return <p className='text-center text-gray-500'>No Notes Yet</p>;
+    return <p className='text-center text-gray-500'>No Notes Yet test</p>;
   }
 
   return (
